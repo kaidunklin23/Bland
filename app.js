646175a4893fedaadcbf49328db4a2050328a453
Kaija Dunklin <kaykay_s@yahoo.com>
2020-07-10 17:07:09 -0400

    7/10 HW

--- app.js
+++ app.js
@@ -4,6 +4,32 @@ var ke = 15;
 function sum(ka, ke){
     return (ka + ke);
 } 
-
 console.log(sum(ka,ke));
 
+function dif(ka,ke){
+    return (ka - ke);
+}
+console.log(dif(ka,ke));
+
+function mul(ka,ke){
+    return (ka*ke);
+}
+console.log(mul(ka,ke));
+
+function div(ka,ke){
+    return (ka/ke);
+}
+console.log(div(ka,ke));
+
+var sum = (ka,ke) => {
+    return (ka + ke);
+}
+var dif = (ka,ke) => {
+    return (ka - ke);
+}
+var mul = (ka,ke) => {
+    return (ka*ke);
+}
+var div = (ka,ke) => {
+    return (ka/ke);
+}
